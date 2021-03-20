ecb25c59b5700f31f387cac8040df2122c435efe
jqteh <64167219+jqteh@users.noreply.github.com>
2021-03-20 09:41:38 +0000

    fix scrolling issue

--- extension/content.js
+++ extension/content.js
@@ -10,17 +10,22 @@ document.addEventListener('mouseup', function (e) {
         renderBubble(e.clientX, e.clientY, selection);
     }
 
-},false)
+}, false)
 
 // Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function (e) {
     bubbleDOM.style.visibility = 'hidden';
-  }, false);
-  
-  // Move that bubble to the appropriate location.
-  function renderBubble(mouseX, mouseY, selection) {
+}, false);
+
+var viewportOffset = el.getBoundingClientRect();
+// these are relative to the viewport, i.e. the window
+var top = viewportOffset.top;
+var left = viewportOffset.left;
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
     bubbleDOM.innerHTML = `<h3>Definition</h3><p>${selection}</p>`;
-    bubbleDOM.style.top = mouseY + 'px';
-    bubbleDOM.style.left = mouseX + 'px';
+    bubbleDOM.style.top = mouseY - top + 'px';
+    bubbleDOM.style.left = mouseX - left + 'px';
     bubbleDOM.style.visibility = 'visible';
-  }
+}
