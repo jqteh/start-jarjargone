a4c74217713587c642ee8b9fde196d4254fb0a82
jqteh <64167219+jqteh@users.noreply.github.com>
2021-03-20 17:33:35 +0000

    fix button status persist (same page navigation)

--- extension/content.js
+++ extension/content.js
@@ -2,26 +2,47 @@ var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
 
-var toggle = false
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded',afterDOMLoaded);
+} else {
+    afterDOMLoaded();
+}
+
+function afterDOMLoaded(){
+    //Everything that needs to happen after the DOM has initially loaded.
+    console.log('loaded')
+    chrome.runtime.sendMessage({
+        type:"getToggle"
+    }, function(response){
+        console.log("The current toggle_on is:" + response.result);
+        if (response.result === "toggle_on") {
+            document.addEventListener('mouseup', mouseUp);
+            document.addEventListener('mousedown', mouseDown);
+        } else if (response.result === "toggle_off") {
+            console.log("default: toggle-off")
+        }
+    })
+}
+
 
 // Receive instructions from background.js (extension button)
 chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
 
-    if (msg.action == 'toggle') {
+    if (msg.action == 'toggle_on') {  // Turn on functionality
 
-        toggle = !toggle;
-        console.log(toggle)
+        console.log(msg.action)
 
-        if (toggle) { // Turn on functionality
             // Open bubble on text selection
-            document.addEventListener('mouseup', mouseUp)
+            document.addEventListener('mouseup', mouseUp);
 
             // Close the bubble when we click on the screen.
             document.addEventListener('mousedown', mouseDown);
-        } else { // Turn off functionality
-            document.removeEventListener('mouseup', mouseUp);
-            document.removeEventListener('mousedown', mouseUp);
-        }
+        
+    } else if (msg.action == 'toggle_off') { // Turn off functionality
+        console.log(msg.action)
+
+        document.removeEventListener('mouseup', mouseUp);
+        document.removeEventListener('mousedown', mouseUp);
     }
 
     // return true
@@ -32,6 +53,9 @@ function mouseUp(e) {
 
     if (selection.length) {
         console.log(selection);
+        var top = e.clientY + window.scrollY;
+        var left = e.clientX + window.scrollX;
+        renderBubble(left, top, selection);
 
         //Post to server
         // chrome.runtime.sendMessage(
@@ -43,10 +67,10 @@ function mouseUp(e) {
         //         debugger;
         //         if (response != undefined && response != "") {
         //             console.log(response)
-             
+
         //             var top = e.clientY + window.scrollY;
         //             var left = e.clientX + window.scrollX;
-            
+
         //             renderBubble(left, top, response);
         //         } else {
         //             debugger;
@@ -56,25 +80,25 @@ function mouseUp(e) {
         // )
 
         // Test API merriam webster medical dictionary
-        chrome.runtime.sendMessage(
-            {
-                contentScriptQuery: "getData",
-                data: JSON.stringify(selection),
-                url: `https://www.dictionaryapi.com/api/v3/references/medical/json/${selection}?key=hidden`
-            }, function (response) {
-                if (response != undefined && response != "") {
-                    console.log(response)
-             
-                    var top = e.clientY + window.scrollY;
-                    var left = e.clientX + window.scrollX;
-            
-                    renderBubble(left, top, response);
-                } else {
-                    debugger;
-                    console.log("problem")
-                }
-            }
-        )
+        // chrome.runtime.sendMessage(
+        //     {
+        //         contentScriptQuery: "getData",
+        //         data: JSON.stringify(selection),
+        //         url: `https://www.dictionaryapi.com/api/v3/references/medical/json/${selection}?key=hidden`
+        //     }, function (response) {
+        //         if (response != undefined && response != "") {
+        //             console.log(response)
+
+        //             var top = e.clientY + window.scrollY;
+        //             var left = e.clientX + window.scrollX;
+
+        //             renderBubble(left, top, response);
+        //         } else {
+        //             debugger;
+        //             console.log("problem")
+        //         }
+        //     }
+        // )
 
     }
 }
