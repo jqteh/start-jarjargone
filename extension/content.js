8cc7c3d12774d9b10a5f7ed00b6e1d68d8e06caa
jqteh <64167219+jqteh@users.noreply.github.com>
2021-03-21 02:40:35 +0000

    write word length logic

--- extension/content.js
+++ extension/content.js
@@ -2,18 +2,18 @@ var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
 
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded',afterDOMLoaded);
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', afterDOMLoaded);
 } else {
     afterDOMLoaded();
 }
 
-function afterDOMLoaded(){
+function afterDOMLoaded() {
     //Everything that needs to happen after the DOM has initially loaded.
     console.log('loaded')
     chrome.runtime.sendMessage({
-        type:"getToggle"
-    }, function(response){
+        type: "getToggle"
+    }, function (response) {
         console.log("The current toggle_on is:" + response.result);
         if (response.result === "toggle_on") {
             document.addEventListener('mouseup', mouseUp);
@@ -32,12 +32,12 @@ chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
 
         console.log(msg.action)
 
-            // Open bubble on text selection
-            document.addEventListener('mouseup', mouseUp);
+        // Open bubble on text selection
+        document.addEventListener('mouseup', mouseUp);
 
-            // Close the bubble when we click on the screen.
-            document.addEventListener('mousedown', mouseDown);
-        
+        // Close the bubble when we click on the screen.
+        document.addEventListener('mousedown', mouseDown);
+
     } else if (msg.action == 'toggle_off') { // Turn off functionality
         console.log(msg.action)
 
@@ -50,9 +50,11 @@ chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
 
 function mouseUp(e) {
     var selection = window.getSelection().toString();
+    var words = selection.split(" ");
 
     if (selection.length) {
         console.log(selection);
+        console.log(words.length);
 
         //Mirror selection - no processing
         // var top = e.clientY + window.scrollY;
@@ -60,46 +62,46 @@ function mouseUp(e) {
         // renderBubble(left, top, selection);
 
         //Post to server
-        chrome.runtime.sendMessage(
-            {
-                contentScriptQuery: "postData",
-                data: JSON.stringify(selection),
-                url: "http://127.0.0.1:5000/api"
-            }, function (response) {
-                if (response != undefined && response != "") {
-                    console.log(response)
+        if (words.length > 3) {
+            chrome.runtime.sendMessage(
+                {
+                    contentScriptQuery: "postData",
+                    data: JSON.stringify(selection),
+                    url: "http://127.0.0.1:5000/api"
+                }, function (response) {
+                    if (response != undefined && response != "") {
+                        console.log(response)
 
-                    var top = e.clientY + window.scrollY;
-                    var left = e.clientX + window.scrollX;
+                        var top = e.clientY + window.scrollY;
+                        var left = e.clientX + window.scrollX;
 
-                    renderBubble(left, top, response);
-                } else {
-                    console.log("problem")
+                        renderBubble(left, top, response);
+                    } else {
+                        console.log("problem")
+                    }
                 }
-            }
-        )
+            )
+        } else {
+            // Test API merriam webster medical dictionary
+            chrome.runtime.sendMessage(
+                {
+                    contentScriptQuery: "getData",
+                    data: JSON.stringify(selection),
+                    url: `https://www.dictionaryapi.com/api/v3/references/medical/json/${selection}?key=096aed10-46a0-4079-b2d4-2fbfec95e4d7`
+                }, function (response) {
+                    if (response != undefined && response != "") {
+                        console.log(response)
 
-        // Test API merriam webster medical dictionary
-        // chrome.runtime.sendMessage(
-        //     {
-        //         contentScriptQuery: "getData",
-        //         data: JSON.stringify(selection),
-        //         url: `https://www.dictionaryapi.com/api/v3/references/medical/json/${selection}?key=hidden`
-        //     }, function (response) {
-        //         if (response != undefined && response != "") {
-        //             console.log(response)
+                        var top = e.clientY + window.scrollY;
+                        var left = e.clientX + window.scrollX;
 
-        //             var top = e.clientY + window.scrollY;
-        //             var left = e.clientX + window.scrollX;
-
-        //             renderBubble(left, top, response);
-        //         } else {
-        //             debugger;
-        //             console.log("problem")
-        //         }
-        //     }
-        // )
-
+                        renderBubble(left, top, response);
+                    } else {
+                        console.log("problem")
+                    }
+                }
+            )
+        }
     }
 }
 
